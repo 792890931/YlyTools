d0a72e558d52be44271646f859aadb86b38da067
Hongtao.cheng <clientau@163.com>
2022-03-03 19:22:54 +0800

    init

---
+++ src/SayHello.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This is not a free software, can not be copied and/or distributed.
+ * @copyright   翎医信息科技（上海）有限公司 ( http://www.doctopia.com.cn/)
+ * @filename：  SayHello.php
+ * @author：    Clien <clien.chen@doctopia.com.cn>
+ * @datetime：  2022/3/3 19:15
+ * @license    http://www.doctopia.com.cn/
+ * @since      Version 1.0.0
+ */
+
+namespace Yly101\Tools;
+
+class SayHello{
+    public static function world(){
+        echo rand(10000,888888);
+    }
+}
---
+++ test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This is not a free software, can not be copied and/or distributed.
+ * @copyright   翎医信息科技（上海）有限公司 ( http://www.doctopia.com.cn/)
+ * @filename：  test.php
+ * @author：    Clien <clien.chen@doctopia.com.cn>
+ * @datetime：  2022/3/3 19:15
+ * @license    http://www.doctopia.com.cn/
+ * @since      Version 1.0.0
+ */
+
+require_once __DIR__."/vendor/autoload.php";
+use Yly101\Tools\SayHello;
+SayHello::world();
